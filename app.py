e831ca1e2821a3e490766699bf9823be06def083
Raynor <84258177+RayByteo@users.noreply.github.com>
2024-01-29 20:57:05 -0500

    idk

--- app.py
+++ app.py
@@ -3,19 +3,27 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    quote = ""
-    error_message = ""
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_country_info', methods=['POST'])
+def get_country_info():
     if request.method == 'POST':
-        if request.form['submit_button'] == 'Generate':
-            response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
-            if response.status_code == 200:
-                data = response.json()
-                quote = data['title'] if data else ""
-            else:
-                error_message = "Error getting quote"
-    return render_template('index.html', quote=quote, error_message=error_message)
+        country_name = request.form['country_name']
+        country_info = fetch_country_info(country_name)
+        return render_template('index.html', country_info=country_info)
+
+def fetch_country_info(country_name):
+    base_url = 'https://restcountries.com/v2/name/'
+    url = f'{base_url}{country_name}'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        country_info = response.json()
+        return country_info
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
